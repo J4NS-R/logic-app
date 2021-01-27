c240ca667a2638987e47d48db8b9d5b6c2151e14
Jans Rautenbach <git@moat.rauten.co.za>
2021-01-27 16:41:33 +0200

    Removed localhost hardcode and cleaned up tracing statements

--- za.org.cair.logic_app.web/src/main/java/za/org/cair/logic_app/web/Translate.java
+++ za.org.cair.logic_app.web/src/main/java/za/org/cair/logic_app/web/Translate.java
@@ -18,24 +18,25 @@ import za.org.cair.logic_app.generator.Main;
 
 
 // Code for this endpoint adapted from https://dzone.com/articles/develop-a-rest-api-using-java-and-jetty
+// TODO change to "Compile" instead of legacy "Translate"
 @WebServlet(name = "TranslateServlet", urlPatterns = {"translate"}, loadOnStartup = 1)
 public class Translate extends HttpServlet {
 	
-    /**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
 		String input = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+		System.out.println("=== Received for compilation:");
+		System.out.println(input);
 		String toReturn = "";
 		try {
 			toReturn = ExternalCompiler.compileToString(input);			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		System.out.println("value being returned is \n" + toReturn);
+		System.out.println("=== Returning:");
+		System.out.println(toReturn);
 		response.getWriter().print(toReturn);
 	}
 }
